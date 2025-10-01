e99d36dee5eb942cd1fd15f9bab85c224e6b86af
SeanHgithub <seanhadley11@gmail.com>
2025-10-01 09:57:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 tallenough = True
 withadult = True
-
+healthhold = False
+if healthhold == True:
+    print("you can not enter")
+elif withadult or tallenough == True:
+    print("you can ride")
 
  
